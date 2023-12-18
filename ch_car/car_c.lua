cab865eb23085922910343c3ea1d372a9e114197
kelson8 <kelson@kelsoncraft.net>
2023-12-18 17:50:39 -0500

    Move items into functions in car_c.lua.

--- ch_car/car_c.lua
+++ ch_car/car_c.lua
@@ -1,3 +1,44 @@
+-- TODO Setup toggle for being in the driver seat or spawning beside the player.
+
+function notify(msg)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(msg)
+    DrawNotification(true, false)
+end
+
+function spawnVehicle(vehicleName)
+    local car = GetHashKey(vehicleName)
+
+    -- Check if the vehicle actually exists
+    if not IsModelInCdimage(vehicleName) or not IsModelAVehicle(vehicleName) then
+    notify("~r~Error~w~: The model " .. vehicleName .. " doesn't exist!")
+    end
+
+    -- Load the model
+    RequestModel(vehicleName)
+
+    -- If model hasn't loaded, wait on it.
+    while not HasModelLoaded(vehicleName) do
+        Wait(500)
+    end
+
+    local ped = GetPlayerPed(-1)
+    local x,y,z = table.unpack(GetEntityCoords(ped))
+    local heading = GetEntityHeading(ped)
+
+    local vehicle = CreateVehicle(vehicleName, x, y, z, heading, true, false)
+    -- local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1)))
+
+    -- Set the player into the drivers seat
+    SetPedIntoVehicle(ped, vehicle, -1)
+
+    -- give the vehicle back to the game (This'll make the game decide when to despawn the vehicle)
+    SetEntityAsNoLongerNeeded(vehicle)
+
+    -- Release the model
+    SetModelAsNoLongerNeeded(vehicle)
+end
+
 RegisterCommand('car_menu', function (source, args)
     -- TODO figure out how to make a menu with a couple of cars in it.
 end)
@@ -6,42 +47,17 @@ RegisterCommand('car', function(source, args)
     -- account for the argument not being passed
     local vehicleName = args[1] or 'adder'
 
-    -- check if the vehicle actually exists
-    if not IsModelInCdimage(vehicleName) or not IsModelAVehicle(vehicleName) then
-        TriggerEvent('chat:addMessage', {
-            args = { 'Vehicle not recognized ' .. vehicleName }
-        })
-        return
-    end
+    -- Spawn the car
+    spawnVehicle(vehicleName)
 
-    -- load the model
-    RequestModel(vehicleName)
-
-    -- wait for the model to load
-    while not HasModelLoaded(vehicleName) do
-        Wait(500) -- often you'll also see Citizen.Wait
-    end
-
-    -- get the player's position
-    local playerPed = PlayerPedId() -- get the local player ped
-    local pos = GetEntityCoords(playerPed) -- get the position of the local player ped
-
-    -- create the vehicle
-    local vehicle = CreateVehicle(vehicleName, pos.x, pos.y, pos.z, GetEntityHeading(playerPed), true, false)
-
-    -- set the player ped into the vehicle's driver seat
-    SetPedIntoVehicle(playerPed, vehicle, -1)
-
-    -- give the vehicle back to the game (this'll make the game decide when to despawn the vehicle)
-    SetEntityAsNoLongerNeeded(vehicle)
-
-    -- release the model
-    SetModelAsNoLongerNeeded(vehicleName)
+    -- Set the player into the drivers seat
+    --SetPedIntoVehicle(ped, vehicleName, -1)
 
     -- tell the player
-    TriggerEvent('chat:addMessage', {
-		args = { 'You have spawned a ^* ^3' .. vehicleName }
-	})
+    notify("You have spawned a ~y~" .. vehicleName)
+    -- TriggerEvent('chat:addMessage', {
+	-- 	args = { 'You have spawned a ^* ^3' .. vehicleName }
+	-- })
 end, false)
 
 RegisterCommand('dv', function()
