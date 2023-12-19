39a78942700b305f9d8068e47b0b9e7e0b9da9a0
kelson8 <kelson@kelsoncraft.net>
2023-12-18 19:02:47 -0500

    Add base for ch_nui.

---
+++ ch_nui/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+client_scripts {
+    'nui_c.lua',
+}
+
+-- NUI
+ui_page 'nui/nui.html'
+
+files {
+    'nui/nui.html',
+    'nui/style.css',
+    'nui/script.js',
+    --'nui/images/*'
+}
