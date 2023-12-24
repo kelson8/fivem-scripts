079f14ff4f7ca3d9b897108f140c1573fc09a57c
kelson8 <kelson@kelsoncraft.net>
2023-12-24 13:38:18 -0500

    Add routing bucket test.

---
+++ ch_test/client/test.lua
@@ -0,0 +1,23 @@
+-- This looks like it could be fun, set player invincible with ragdoll enabled.
+-- SetPlayerInvincibleKeepRagdollEnabled(player)
+
+-- I wonder how this one works.
+-- https://pastebin.com/GBnsQ5hr
+-- PlayPoliceReport()
+
+-- IsPlayerDrivingDangerously()
+
+--[[
+    SetPlayerMayNotEnterAnyVehicle()
+    SetPlayerMayOnlyEnterThisVehicle()
+]]
+
+
+-- Try and play around with routing buckets, those might be fun to screw around with
+--[[
+SetEntityRoutingBucket(entity, bucket)
+SetPlayerRoutingBucket(player, bucket)
+
+GetPlayerRoutingBucket(player)
+GetEntityRoutingBucket(entity)
+]]
--- ch_test/fxmanifest.lua
+++ ch_test/fxmanifest.lua
@@ -3,6 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 client_scripts{
+    'client/lobby_test.lua',
     'client/spawntables.lua',
     'client/text.lua',
 }
