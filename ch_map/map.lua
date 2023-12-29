ed7f5dab7e67988daae88209ca2e2ec494324a10
kelson8 <kelson@kelsoncraft.net>
2023-12-29 16:47:36 -0500

    Add more spawn locations, add spawn list.

--- ch_map/map.lua
+++ ch_map/map.lua
@@ -1,3 +1,9 @@
-spawnpoint 'player_one' { x = 222.2027, y = -864.0162, z = 30.2922 } -- Franklin in the middle of Los Santos
-spawnpoint 'player_one' {x = -1853.68, y = 2815.78, z = 32.81 } -- Franklin at the army base
--- spawnpoint 'player_one' {x = -1853.68, y = 2815.78, z = 32.81 } -- Franklin at the army base
+-- Todo, setup this to where it grabs values from a list of spawn points defined in a file.
+-- player_one is franklin
+-- Los Santos
+spawnpoint 'player_one' { x = 222.2027, y = -864.0162, z = 30.2922 } -- Middle of Los Santos
+spawnpoint 'player_one' {x = 925.329, y = 46.152, z = 80.908 } -- Casino
+spawnpoint 'player_one' {x = -695.025, y = 82.955, z = 55.855 } -- Epsilon Building
+--spawnpoint 'player_one' {x = -1330.911, y = 340.871, z = 64.078} -- Richman Hotel
+
+--spawnpoint 'player_one' {x = -1853.68, y = 2815.78, z = 32.81 } -- Army base
