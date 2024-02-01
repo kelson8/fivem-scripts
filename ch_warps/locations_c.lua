fb6373f340069d795957445b5067f26f8be397b3
kelson8 <kelson@kelsoncraft.net>
2024-02-01 00:10:04 -0500

    Add ch_warps, update gitignore.

---
+++ ch_warps/locations_c.lua
@@ -0,0 +1,51 @@
+locations = {
+    {
+        pos = {x = -676.2, y = 94.2, z = 54.5},
+        tpto = {x = -663.1, y = 77.81, z = 50.25},
+        submarker = {
+            marker = 29,
+            posz = 15.25,
+        },
+        marker = 1,
+        scale = 2.0,
+        rgba = {120, 255, 120, 155}
+    },
+    {
+        pos = {x = -657.97, y = 84.14, z = 52.06},
+        tpto = {x = -640.92, y = 82.23, z = 51.44},
+        submarker = {
+            marker = 29,
+            posz = 15.25,
+        },
+        marker = 1,
+        scale = 2.0,
+        rgba = {120, 255, 120, 155}
+    },
+
+    {
+        pos = {x = -666.15, y = 46.65, z = 40.40},
+        tpto = {x = -618.3, y = 34.47, z = 43.53},
+        submarker = {
+            marker = 29,
+            poz = 15.25,
+        },
+        marker = 1,
+        scale = 2.0,
+        rgba = {120, 255, 120, 155}
+    },
+
+    -- FIB Building Los Santos
+    -- X = 121.6, Y = -759.33, Z = 45.75
+    {
+        -- Middle of Los Santos marker #1 teleport to FIB Building
+        pos = {x = 236.2, y = -874.9, z = 29.49},
+        tpto = {x = 121.6, y = -759.33, z = 45.75},
+        submarker = {
+            marker = 29,
+            poz = 15.25,
+        },
+        marker = 1,
+        scale = 2.0,
+        rgba = {120, 255, 120, 155}
+    },
+}
---
+++ ch_warps/warps_c.lua
@@ -0,0 +1,56 @@
+function notify(msg)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(msg)
+    DrawNotification(true, false)
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(1)
+        for i = 1, #locations, 1 do
+            loc = locations[i]
+            DrawMarker(
+                loc.marker,
+                loc.pos.x,
+                loc.pos.y,
+                loc.pos.z,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                loc.scale,
+                loc.scale,
+                loc.scale,
+                loc.rgba[1],
+                loc.rgba[2],
+                loc.rgba[3],
+                loc.rgba[4],
+                false, true, 2, nil, nil, false
+            )
+            if loc.submarker ~= nil then DrawMarker(
+                loc.submarker.marker,loc.pos.x,loc.pos.y,loc.submarker.posz,
+                0.0,0.0,0.0,0.0,0.0,0.0,
+                loc.scale/2,loc.scale/2,loc.scale/2,
+                loc.rgba[1],loc.rgba[2],loc.rgba[3],loc.rgba[4],
+                false,true,2,nil,nil,false)
+            end
+            local playerCoord = GetEntityCoords(PlayerPedId(), false)
+            local locVector = vector3(loc.pos.x, loc.pos.y, loc.pos.z)
+            if Vdist2(playerCoord, locVector) < loc.scale * 1.12 and GetVehiclePedIsIn(PlayerPedId(), false) == 0 then
+                -- Code that runs when we are in the markers radius.
+                SetEntityCoords(PlayerPedId(), loc.tpto.x, loc.tpto.y, loc.tpto.z, true, true, true, false)
+                SetEntityHeading(PlayerPedId(), 0)
+                notify("You have been teleported to location ~b~" .. i)
+            end
+        end
+    end
+end)
+
+-- RegisterCommand("ott", function()
+--     for i = 1, #locations, 1 do
+--         loc = locations[i]
+--     end
+--     notify(loc.pos.z)
+-- end)
